f1ae7c63717cac662fb3c6fd678ce40d99698270
un3ch1 <uniquechl@outlook.com>
2023-04-12 13:51:21 +0800

    重命名项目

--- os_compose.py
+++ os_compose.py
@@ -34,13 +34,14 @@ def create_vm(conn, name, image, flavor, networks):
             #networks=[{"uuid": net_id, "fixed_ip": ip_address}],
         )
 
-        # 等待 VM 启动并获取 IP 地址
-        server = conn.compute.wait_for_server(server)
+        # 等待 VM 启动并获取 IP 地址,最多等180秒
+        server = conn.compute.wait_for_server(server=server, wait=180)
         #network = conn.network.find_network(name_or_id=net_id)
         ip_address = server.addresses
 
         # 打印 VM IP 地址
-        print(f"Created VM {name} with IP address {ip_address} and password: {server.admin_password}")
+        print(f"Created VM {name} with IP address {ip_address}\
+             and password: {server.admin_password}")
     except openstack.exceptions.BadRequestException as error:
         print(error)
         # 获取镜像和配额对象
@@ -65,30 +66,26 @@ def create_vm(conn, name, image, flavor, networks):
         ip_address = server.addresses
 
         # 打印 VM IP 地址
-        print(f"Created VM {name} with IP address {ip_address} and password: {server.admin_password}")
+        print(f"Created VM {name} with IP address {ip_address}\
+             and password: {server.admin_password}")
+    except openstack.exceptions.ResourceTimeout:
+        print("Created VM waiting timeout")
 
 
-def create_subnet(conn, network, cidr_prefix, gw_ip, dhcp_ip_range):
+def create_subnet(conn, network, cidr_prefix, gw_ip):
     """定义创建 Subnet 的函数，将需要动态计算 CIDR 的变量作为参数传递"""
-    # 连接 OpenStack
-
-
-    # 设定 DHCP IP 地址的数量，这里给出的是 10.0.0.2~10.0.0.254 地址池大小
-    dhcp_ips = len(netaddr.IPNetwork(dhcp_ip_range)) - 1
 
     # 设定Subnet 的掩码
     cidr = netaddr.IPNetwork(cidr_prefix)
-    mask_bits = cidr.prefixlen
-    subnet_mask = netaddr.IPAddress((1 << 32) - (1 << 32 - mask_bits))
+    #mask_bits = cidr.prefixlen
+    #subnet_mask = netaddr.IPAddress((1 << 32) - (1 << 32 - mask_bits))
 
     # 计算 Subnet 网段和广播地址
-    subnet_start = netaddr.IPAddress(cidr.network + 1)
-    subnet_end = netaddr.IPAddress(cidr.broadcast - 2)
+    subnet_start = netaddr.IPAddress(cidr.network + 2)
+    subnet_end = netaddr.IPAddress(cidr.broadcast - 1)
 
     # 根据掩码位和 IP 范围大小计算 DHCP 可用 IP 地址
-    dhcp_start = netaddr.IPAddress(
-        cidr.network + 2
-    )
+    dhcp_start = subnet_start
     dhcp_end = subnet_end
 
     # 创建 Subnet
@@ -118,9 +115,12 @@ def find_subnet(connection, cidr):
 def create_router(connection, subnets, external_network_name=None):
     """创建路由，连接指定子网"""
     router_name = 'auto-created-router'
-    if external_network_name != None:
+    if external_network_name is not None:
         external_network = connection.network.find_network(external_network_name)
-        router = connection.network.create_router(name=router_name, external_gateway_info={'network_id': external_network.id})
+        router = connection.network.create_router(
+            name=router_name,
+            external_gateway_info={'network_id': external_network.id}
+            )
     else:
         router = connection.network.create_router(name=router_name)
     print(f"Created router '{router.name}' with ID '{router.id}'")
@@ -163,6 +163,32 @@ def delete_project(connection, project_id):
     """删除project"""
     connection.identity.delete_project(project_id)
 
+def create_networks(connection, network_config, vmnet_list):
+    """根据网络配置创建网络及子网,并返回openstack网络配置"""
+    subnets = {}
+    networks = []
+    # 遍历配置文件中的net列表，获取 IP 地址和掩码
+    for vmnet in vmnet_list:
+        ip_net = netaddr.IPNetwork(network_config[vmnet]["ip_addr"])
+
+        cidr_prefix = str(ip_net.cidr)
+
+        # 检查子网是否存在，如果不存在则创建
+        try:
+            subnet = find_subnet(connection, cidr_prefix)
+        except openstack.exceptions.NotFoundException:
+            network_name = f"auto-created-network-{cidr_prefix}"
+            network = connection.network.create_network(name=network_name)
+            # 计算网关ip
+            gw_ip = str(netaddr.IPAddress(ip_net.first + 1))
+
+
+            subnet = create_subnet(connection, network, cidr_prefix, gw_ip)
+
+        # 将子网对象添加到 subnets 列表中
+        subnets[str(ip_net)] = subnet
+        networks.append({"uuid": subnet.network_id, "fixed_ip": ip_net.ip})
+        return networks, subnets
 
 
 def main():
@@ -174,52 +200,19 @@ def main():
     project_name = vm_configs['project']['name']
     # 连接 OpenStack，创建一个空 subnet_id 列表以存储创建的子网 ID
     admin_connection = openstack.connect(**auth_args)
-    subnets = {}
     # 可互通网络列表
     interoperable = []
 
     # 创建指定项目，并返回新项目的连接对象
     connection, project = create_project(admin_connection, project_name)
-    
     # 从配置文件中读取网络信息
     network_config = vm_configs['project']['nets']
 
     # 处理虚拟机配置
     for config in vm_configs['project']['vm']:
-        networks = []
         # 处理虚拟机网络配置
         vmnet_list = [net_name for net_name in config['net']]
-
-        # 遍历配置文件中的net列表，获取 IP 地址和掩码
-        for vmnet in vmnet_list:
-            ip_net = netaddr.IPNetwork(network_config[vmnet]["ip_addr"])
-    
-            cidr_prefix = str(ip_net.cidr)
-
-            # 检查子网是否存在，如果不存在则创建
-            try:
-                subnet = find_subnet(connection, cidr_prefix)
-            except openstack.exceptions.NotFoundException:
-                network_name = f"auto-created-network-{cidr_prefix}"
-                network = connection.network.create_network(name=network_name)
-
-                gw_ip = str(netaddr.IPAddress(ip_net.first + 254))
-
-                dhcp_ip_range = str(
-                    netaddr.IPNetwork(
-                        f'{netaddr.IPAddress(ip_net.first + 2)}/{ip_net.prefixlen}'
-                        )
-                )
-                subnet = create_subnet(
-                    connection, network, cidr_prefix, gw_ip, dhcp_ip_range
-                    )
-
-            # 将子网对象添加到 subnets 列表中
-            subnets[str(ip_net)] = subnet
-            networks.append({"uuid": subnet.network_id, "fixed_ip": ip_net.ip})
-
-            
-
+        networks, subnets = create_networks(connection,network_config,vmnet_list)
         # 2.创建 VM
         create_vm(
             conn=connection,
