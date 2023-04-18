d05adce3fe0da611725fb175077f4ddd3e191938
un3ch1 <uniquechl@outlook.com>
2023-04-18 16:10:57 +0800

    实现创建安全组和安全组规则

--- libs/net.py
+++ libs/net.py
@@ -4,11 +4,13 @@ class Net:
     cidr = ''
     subnet = ''
     network = ''
+    allow_ports = [22,80,443,445,3389,8080,5901]
 
 
     def __init__(self,cfg:dict) -> None:
         self.name = cfg['name']
         self.ip_range = cfg['ip_range']
+        self.allow_ports = cfg['ports']
     
     def update(self) -> None:
         """接受创建的Network对象"""
--- libs/vm.py
+++ libs/vm.py
@@ -5,12 +5,15 @@ class VM:
     flavor = ''
     server = ''
     float_ip = ''
+    allow_ports = [22,80,443,445,3389,8080,5901]
+    sec_group = []
     def __init__(self,cfg:dict) -> None:
         self.ip_address = []
         self.networks = []
         self.name = cfg['name']
         self.image = cfg['image']
         self.flavor = cfg['flavor']
+        #self.allow_ports = cfg['ports']
         if 'ip_address' in cfg.keys():
             for ip in cfg['ip_address']:
                 self.ip_address.append(netaddr.IPNetwork(ip))
